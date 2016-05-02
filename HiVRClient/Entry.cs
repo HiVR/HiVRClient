4ffbc64db5c9bd39bde6fc4cf0b1fbd2b4fdf096
Wim de With <nauxuron@wimdewith.com>
2016-05-02 18:45:10 +0200

    Add project files.

---
+++ HiVRClient/Entry.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="Entry.cs" company="HiVR">
+// Copyright (c) 2016 HiVR All Rights Reserved
+// </copyright>
+namespace HiVRClient
+{
+    using System;
+
+    /// <summary>
+    /// Class containing the entry point of the application
+    /// </summary>
+    public static class Entry
+    {
+        #region Methods
+
+        /// <summary>
+        /// The entry point of the application
+        /// </summary>
+        /// <param name="args">The arguments passed to the program</param>
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            HiVRClient app = new HiVRClient();
+            app.Run();
+        }
+
+        #endregion Methods
+    }
+}
---
+++ HiVRClient/View/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="MainWindow.xaml.cs" company="HiVR">
+// Copyright (c) 2016 HiVR All Rights Reserved
+// </copyright>
+namespace HiVRClient
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow" /> class
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion Constructors
+    }
+}
