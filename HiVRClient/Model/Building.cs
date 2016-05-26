3dd44b2ab6a57eb4f7622a85345d2e0f3d46e832
Wim de With <nauxuron@wimdewith.com>
2016-05-26 11:54:26 +0200

    Fix StyleCop warnings

--- HiVRClient/Model/Building.cs
+++ HiVRClient/Model/Building.cs
@@ -1,9 +1,22 @@
-﻿namespace HiVRClient.Model
+﻿// <copyright file="Building.cs" company="HiVR">
+// Copyright (c) 2016 HiVR All Rights Reserved
+// </copyright>
+namespace HiVRClient.Model
 {
+    /// <summary>
+    /// This class implements a Building
+    /// </summary>
     public class Building : Drawable
     {
         #region Constructors
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Building"/> class
+        /// </summary>
+        /// <param name="id">The id of the drawable building</param>
+        /// <param name="xlocation">The x coordinate of the building</param>
+        /// <param name="ylocation">The y coordinate of the building</param>
+        /// <param name="zlocation">The z coordinate of the building</param>
         public Building(int id, double xlocation, double ylocation, double zlocation) : base(id, DrawableType.Building, xlocation, ylocation, zlocation)
         {
         }
--- HiVRClient/Model/DrawableType.cs
+++ HiVRClient/Model/DrawableType.cs
@@ -1,7 +1,16 @@
-﻿namespace HiVRClient.Model
+﻿// <copyright file="DrawableType.cs" company="HiVR">
+// Copyright (c) 2016 HiVR All Rights Reserved
+// </copyright>
+namespace HiVRClient.Model
 {
+    /// <summary>
+    /// This enumeration contains drawable types for the view to determine how to draw
+    /// </summary>
     public enum DrawableType
     {
+        /// <summary>
+        /// The building type
+        /// </summary>
         Building,
-    };
+    }
 }
