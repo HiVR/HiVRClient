bd09fb8d254cf4888b3dc92df1a396423acba677
Wim de With <nauxuron@wimdewith.com>
2016-05-26 14:20:23 +0200

    Update comments to reflect @vennik code review

--- HiVRClient/Model/Building.cs
+++ HiVRClient/Model/Building.cs
@@ -4,19 +4,19 @@
 namespace HiVRClient.Model
 {
     /// <summary>
-    /// This class implements a Building
+    /// This class implements a Building.
     /// </summary>
     public class Building : Drawable
     {
         #region Constructors
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Building"/> class
+        /// Initializes a new instance of the <see cref="Building"/> class.
         /// </summary>
-        /// <param name="id">The id of the drawable building</param>
-        /// <param name="xlocation">The x coordinate of the building</param>
-        /// <param name="ylocation">The y coordinate of the building</param>
-        /// <param name="zlocation">The z coordinate of the building</param>
+        /// <param name="id">the id of the drawable building</param>
+        /// <param name="xlocation">the x coordinate of the building</param>
+        /// <param name="ylocation">the y coordinate of the building</param>
+        /// <param name="zlocation">the z coordinate of the building</param>
         public Building(int id, double xlocation, double ylocation, double zlocation) : base(id, DrawableType.Building, xlocation, ylocation, zlocation)
         {
         }
--- HiVRClient/ViewModel/MapViewModel.cs
+++ HiVRClient/ViewModel/MapViewModel.cs
@@ -3,22 +3,21 @@
 // </copyright>
 namespace HiVRClient.ViewModel
 {
-    using Model;
     using System.Collections.ObjectModel;
 
     /// <summary>
-    /// Contains the view model of the actual map
+    /// Contains the view model of the actual map.
     /// </summary>
     public class MapViewModel : BaseViewModel
     {
         #region Constructors
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="MapViewModel"/> class
+        /// Initializes a new instance of the <see cref="MapViewModel"/> class.
         /// </summary>
         public MapViewModel()
         {
-            this.Drawables = new ObservableCollection<DrawableControl>() { new DrawableControl(new Building(0, 100, 100, 100)) };
+            this.Drawables = new ObservableCollection<DrawableControl>();
         }
 
         #endregion Constructors
@@ -26,7 +25,7 @@ namespace HiVRClient.ViewModel
         #region Properties
 
         /// <summary>
-        /// Gets or sets the collection of draw
+        /// Gets or sets the collection of draw.
         /// </summary>
         public ObservableCollection<DrawableControl> Drawables { get; set; }
 
