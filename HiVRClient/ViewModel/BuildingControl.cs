1703945ddbd0fea9e1612e2d7fad5e13c3e37a13
Wim de With <nauxuron@wimdewith.com>
2016-06-02 15:54:03 +0200

    Remove DrawableType enum and implement different DataTemplates

---
+++ HiVRClient/ViewModel/BuildingControl.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="BuildingControl.cs" company="HiVR">
+// Copyright (c) 2016 HiVR All Rights Reserved
+// </copyright>
+namespace HiVRClient.ViewModel
+{
+    using Model;
+
+    /// <summary>
+    /// This class implements the view model for a building.
+    /// </summary>
+    public class BuildingControl : DrawableControl
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BuildingControl"/> class.
+        /// </summary>
+        /// <param name="building">he building model</param>
+        public BuildingControl(Building building) : base(building)
+        {
+        }
+
+        #endregion Constructors
+    }
+}
