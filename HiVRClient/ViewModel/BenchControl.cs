0f61015b7a319158c9d7b8f32a2716ac4353302a
Carlos Brunal <cbabrunal@users.noreply.github.com>
2016-06-15 10:47:31 +0200

    Added controls for car, bench, garden, tv. Updated drawable.xaml aswell. (#24)
    * Added controls for car, bench, garden, tv. Updated drawable.xaml
    
    * Fix television displayed size

---
+++ HiVRClient/ViewModel/BenchControl.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="BenchControl.cs" company="HiVR">
+// Copyright (c) 2016 HiVR All Rights Reserved
+// </copyright>
+namespace HiVRClient.ViewModel
+{
+    using Model;
+
+    /// <summary>
+    /// This class implements the view model for a bench.
+    /// </summary>
+    public class BenchControl : DrawableControl
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BenchControl"/> class.
+        /// </summary>
+        /// <param name="bench">The bench model</param>
+        public BenchControl(Bench bench) : base(bench)
+        {
+        }
+
+        #endregion Constructors
+    }
+}
---
+++ HiVRClient/ViewModel/CarControl.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="CarControl.cs" company="HiVR">
+// Copyright (c) 2016 HiVR All Rights Reserved
+// </copyright>
+namespace HiVRClient.ViewModel
+{
+    using Model;
+
+    /// <summary>
+    /// This class implements the view model for a car.
+    /// </summary>
+    public class CarControl : DrawableControl
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CarControl"/> class.
+        /// </summary>
+        /// <param name="car">The car model</param>
+        public CarControl(Car car) : base(car)
+        {
+        }
+
+        #endregion Constructors
+    }
+}
