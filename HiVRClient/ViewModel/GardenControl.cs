ce1ded71bc814c8e434356fbefd05e9eab48e94e
Carlos Brunal <cbabrunal@gmail.com>
2016-06-15 04:32:26 +0200

    Added controls for car, bench, garden, tv. Updated drawable.xaml

---
+++ HiVRClient/ViewModel/GardenControl.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="GardenControl.cs" company="HiVR">
+// Copyright (c) 2016 HiVR All Rights Reserved
+// </copyright>
+namespace HiVRClient.ViewModel
+{
+    using Model;
+
+    /// <summary>
+    /// This class implements the view model for a tree.
+    /// </summary>
+    public class GardenControl : DrawableControl
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GardenControl"/> class.
+        /// </summary>
+        /// <param name="garden">The Garden model</param>
+        public GardenControl(Garden garden) : base(garden)
+        {
+        }
+
+        #endregion Constructors
+    }
+}
---
+++ HiVRClient/ViewModel/TelevisionControl.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="TelevisionControl.cs" company="HiVR">
+// Copyright (c) 2016 HiVR All Rights Reserved
+// </copyright>
+namespace HiVRClient.ViewModel
+{
+    using Model;
+
+    /// <summary>
+    /// This class implements the view model for a bench.
+    /// </summary>
+    public class TelevisionControl : DrawableControl
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TelevisionControl"/> class.
+        /// </summary>
+        /// <param name="tv">The television model</param>
+        public TelevisionControl(Television tv) : base(tv)
+        {
+        }
+
+        #endregion Constructors
+    }
+}
