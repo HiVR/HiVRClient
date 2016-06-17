f16e62169e686e58c08d9562db990192c1a353d9
Adriaan de Vos <adriaan.devos+git@gmail.com>
2016-06-17 16:29:00 +0200

    Add tests for Model/DrawableModel

---
+++ HiVRClient.Test/Model/DrawableModel/CharacterTest.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="CharacterTest.cs" company="HiVR">
+// Copyright (c) 2016 HiVR All Rights Reserved
+// </copyright>
+using HiVRClient.Model.DrawableModel;
+using NUnit.Framework;
+using System.Windows.Media.Media3D;
+
+namespace HiVRClient.Test.Model.DrawableModel
+{
+    /// <summary>
+    /// Test <see cref="Character"/> class.
+    /// </summary>
+    [TestFixture]
+    public class CharacterTest
+    {
+        #region Methods
+
+        /// <summary>
+        /// Test <see cref="Character.DefaultPosition"/> field.
+        /// </summary>
+        [Test]
+        public void TestDefaultPosition()
+        {
+            Assert.That(Character.DefaultPosition, Is.EqualTo(new Vector3D(0D, 0.9D, 0D)));
+        }
+
+        /// <summary>
+        /// Test <see cref="Character.DefaultRotation"/> field.
+        /// </summary>
+        [Test]
+        public void TestDefaultRotation()
+        {
+            Assert.That(Character.DefaultRotation, Is.EqualTo(new Vector3D(0D, 0D, 0D)));
+        }
+
+        /// <summary>
+        /// Test <see cref="Character.DefaultScale"/> field.
+        /// </summary>
+        [Test]
+        public void TestDefaultScale()
+        {
+            Assert.That(Character.DefaultScale, Is.EqualTo(new Vector3D(0.5D, 1D, 0.5D)));
+        }
+
+        /// <summary>
+        /// Test constructor.
+        /// </summary>
+        [Test]
+        public void TestConstructor()
+        {
+            var bench = new Character(1, new Vector3D(), new Vector3D(), new Vector3D());
+            Assert.That(bench, Is.InstanceOf<Character>());
+        }
+
+        #endregion Methods
+    }
+}
---
+++ HiVRClient.Test/Model/DrawableModel/PatientTest.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="PatientTest.cs" company="HiVR">
+// Copyright (c) 2016 HiVR All Rights Reserved
+// </copyright>
+using HiVRClient.Model.DrawableModel;
+using NUnit.Framework;
+using System.Windows.Media.Media3D;
+
+namespace HiVRClient.Test.Model.DrawableModel
+{
+    /// <summary>
+    /// Test <see cref="Patient"/> class.
+    /// </summary>
+    [TestFixture]
+    public class PatientTest
+    {
+        #region Methods
+
+        /// <summary>
+        /// Test <see cref="Patient.DefaultPosition"/> field.
+        /// </summary>
+        [Test]
+        public void TestDefaultPosition()
+        {
+            Assert.That(Patient.DefaultPosition, Is.EqualTo(new Vector3D(0D, 0.9D, 0D)));
+        }
+
+        /// <summary>
+        /// Test <see cref="Patient.DefaultRotation"/> field.
+        /// </summary>
+        [Test]
+        public void TestDefaultRotation()
+        {
+            Assert.That(Patient.DefaultRotation, Is.EqualTo(new Vector3D(0D, 0D, 0D)));
+        }
+
+        /// <summary>
+        /// Test <see cref="Patient.DefaultScale"/> field.
+        /// </summary>
+        [Test]
+        public void TestDefaultScale()
+        {
+            Assert.That(Patient.DefaultScale, Is.EqualTo(new Vector3D(0.5D, 1D, 0.5D)));
+        }
+
+        /// <summary>
+        /// Test constructor.
+        /// </summary>
+        [Test]
+        public void TestConstructor()
+        {
+            var bench = new Patient(1, new Vector3D(), new Vector3D(), new Vector3D());
+            Assert.That(bench, Is.InstanceOf<Patient>());
+        }
+
+        #endregion Methods
+    }
+}
