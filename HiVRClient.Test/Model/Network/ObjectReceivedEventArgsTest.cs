1759c408428e0eb3561af14242a122d93a2bec7b
Boris Schrijver <bschrijver@schubergphilis.com>
2016-06-17 18:29:59 +0200

    Add Unit tests for ObjectReceivedEventArgs.

---
+++ HiVRClient.Test/Model/Network/ObjectReceivedEventArgsTest.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="ObjectReceivedEventArgsTest.cs" company="HiVR">
+// Copyright (c) 2016 HiVR All Rights Reserved
+// </copyright>
+using HiVRClient.Model.Network;
+using NUnit.Framework;
+using SerializedObjects;
+
+namespace HiVRClient.Test.Model.Network
+{
+    /// <summary>
+    /// Test <see cref="ObjectReceivedEventArgs"/> class.
+    /// </summary>
+    [TestFixture]
+    public class ObjectReceivedEventArgsTest
+    {
+        /// <summary>
+        /// Test constructor.
+        /// </summary>
+        [Test]
+        public void TestConstructor()
+        {
+            var serializableTransformObject = new SerializableTransformObject();
+            var objectReceivedEventArgs = new ObjectReceivedEventArgs(serializableTransformObject);
+            Assert.That(objectReceivedEventArgs, Is.InstanceOf<ObjectReceivedEventArgs>());
+        }
+
+        [Test]
+        public void TestConstructorParameter()
+        {
+            var serializableTransformObject = new SerializableTransformObject();
+            var objectReceivedEventArgs = new ObjectReceivedEventArgs(serializableTransformObject);
+            Assert.That(objectReceivedEventArgs.SerializableTransformObject, Is.EqualTo(serializableTransformObject));
+        }
+
+        [Test]
+        public void TestSerializableTransformObjectProperty()
+        {
+            var serializableTransformObject = new SerializableTransformObject();
+            var objectReceivedEventArgs = new ObjectReceivedEventArgs(serializableTransformObject);
+
+            var serializableTransformObjectTest = new SerializableTransformObject();
+            objectReceivedEventArgs.SerializableTransformObject = serializableTransformObjectTest;
+            Assert.That(objectReceivedEventArgs.SerializableTransformObject, Is.EqualTo(serializableTransformObjectTest));
+        }
+    }
+}
